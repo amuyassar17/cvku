c1296b9deb63e64bee988d67e9e873db5ee379a3
amuyassar17 <amuyassar16@gmail.com>
2020-09-27 03:24:49 +0800

    Updates

---
+++ precache-manifest.8874430784997305958c593955322781.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75fe2cc1a4bf55688afa749691342ed7",
+    "url": "/cvku/index.html"
+  },
+  {
+    "revision": "1f4beb9833f5ff2f0ec3",
+    "url": "/cvku/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "46c9d0c511b1d88d86a1",
+    "url": "/cvku/static/css/main.8562fec9.chunk.css"
+  },
+  {
+    "revision": "1f4beb9833f5ff2f0ec3",
+    "url": "/cvku/static/js/2.8ce07fcc.chunk.js"
+  },
+  {
+    "revision": "f25112c0081db71b0e5dd2ce64e909e6",
+    "url": "/cvku/static/js/2.8ce07fcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46c9d0c511b1d88d86a1",
+    "url": "/cvku/static/js/main.8e9f3872.chunk.js"
+  },
+  {
+    "revision": "a30b040ed7fd5486cac5",
+    "url": "/cvku/static/js/runtime-main.361faa67.js"
+  },
+  {
+    "revision": "8f65ed4eed2408b76e81fd7ec46551d2",
+    "url": "/cvku/static/media/ahmad.8f65ed4e.jpg"
+  }
+]);
